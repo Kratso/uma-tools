32076b63a3d7bf49e0c667402e2eb199ad105f9e
Kratso <kratsothelsamar@gmail.com>
2026-04-11 15:06:09 +0200

    fix: now new umas and skills actually work

--- umalator/app.tsx
+++ umalator/app.tsx
@@ -76,8 +76,8 @@ const enum EventType { CM, LOH }
 
 const presets = (CC_GLOBAL ? [
 	{id: 17, type: EventType.CM, name: 'Virgo Cup', date: '2026-07', courseId: 11103, season: Season.Autumn, ground: GroundCondition.Yielding, weather: Weather.Sunny, time: Time.Midday},
-	{id: 16, type: EventType.CM, name: 'Cancer Cup', date: '2026-06', courseId: 10906, season: Season.Summer, ground: GroundCondition.Yielding, weather: Weather.Cloudy, time: Time.Midday},
-	{id: 15, type: EventType.CM, name: 'Leo Cup', date: '2026-06', courseId: 10501, season: Season.Summer, ground: GroundCondition.Good, weather: Weather.Sunny, time: Time.Midday},
+	{id: 16, type: EventType.CM, name: 'Leo Cup', date: '2026-06', courseId: 10501, season: Season.Summer, ground: GroundCondition.Good, weather: Weather.Sunny, time: Time.Midday},
+	{id: 15, type: EventType.CM, name: 'Cancer Cup', date: '2026-06', courseId: 10906, season: Season.Summer, ground: GroundCondition.Yielding, weather: Weather.Cloudy, time: Time.Midday},
 	{id: 13, type: EventType.CM, name: 'Taurus Cup', date: '2026-05', courseId: 10606, season: Season.Spring, ground: GroundCondition.Good, weather: Weather.Sunny, time: Time.Midday},
 	{id: 14, type: EventType.CM, name: 'Gemini Cup', date: '2026-05', courseId: 10602, season: Season.Spring, ground: GroundCondition.Good, weather: Weather.Sunny, time: Time.Midday},
 	{id: 12, type: EventType.CM, name: 'Aries Cup', date: '2026-04', courseId: 10504, season: Season.Spring, ground: GroundCondition.Good, weather: Weather.Sunny, time: Time.Midday},
